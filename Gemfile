2472f69ea199fb86daf1aeee8eaa6275d105c24d
imkaruna <imkaruna@gmail.com>
2016-06-18 19:39:55 -0400

    Added Bootstrap and modified layouts

--- Gemfile
+++ Gemfile
@@ -22,7 +22,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+#Bootstrap for layout
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'font-awesome-sass'
 # User authorization and authentication using Devise
 gem 'devise'
 # Use ActiveModel has_secure_password
---
+++ app/assets/javascripts/process.js
@@ -0,0 +1,14 @@
+function resetActive(event) {
+    $("div").each(function () {
+        if ($(this).hasClass("activestep")) {
+            $(this).removeClass("activestep");
+        }
+    });
+
+    if (event.target.className == "col-md-2") {
+        $(event.target).addClass("activestep");
+    }
+    else {
+        $(event.target.parentNode).addClass("activestep");
+    }
+}
